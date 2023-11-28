8dc67a738711006cb3c562f6d8b57d4011e55dc3
jeisonTrabajo <jw.vera.r@gmail.com>
2023-11-28 11:52:46 -0500

    modificacion crud de task

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -1,38 +1,63 @@
 import Task from '../models/task.models.js';
 
 export const getTaks=async(req,res)=>{
- const tasks= await Task.find();
+ const tasks= await Task.find({
+    //es para traer las tareas del usuario
+    user:req.user.id
+    //polate para relacionarlo traer sus atos
+ }).populate('user');
 try {
-    res.json(tasks)
+    res.json(tasks);
 } catch (error) {
-    
+    res.status(500).json({message:res.message});
 }
 }
 export const createTask=async(req,res)=>{
+    const {title,description,date}=req.body;
     try {
-        
+      const newTask=new Task({
+          title,
+          description,
+          date,
+          user:req.user.id
+      });
+     const saveTask=await newTask.save();
+
+        res.json(saveTask);
     } catch (error) {
-        
+        res.status(500).json({message:res.message});
     }
 }
 export const getTak=async(req,res)=>{
+    const {id}=req.params;
+
    try {
-    
+    const  task=await Task.findById(id).populate('user');
+    if(!task)return res.status(404);
+    res.json(task); 
+
    } catch (error) {
-    
+    res.status(404).json({message:'ID not found'});
    }
 }
 export const updateTask=async(req,res)=>{
+    const id=req.params.id;
+    
     try {
-        
+        const  task=await Task.findByIdAndUpdate(id,req.body,{new:true});
+        if(!task) return res.status(404).json({message:'ID not found'});
+        res.json(task);
     } catch (error) {
-        
+        res.status(404).json({message:error.message});
     }
 }
 export const deleteTask=async(req,res)=>{
+    const {id}=req.params;
     try {
-        
+        const task= await Task.findOneAndDelete(id);
+       if(!task) return res.status(404).json({message:'ID not found'});
+       res.sendStatus(204);
     } catch (error) {
-        
+        res.status(404).json({message:error.message});
     }
 }
--- src/models/task.models.js
+++ src/models/task.models.js
@@ -12,7 +12,12 @@ const TaskSchema= new mongoose.Schema({
  date:{
     type: Date,
     default:Date.now,
- }
+ },
+ user:{
+   type:mongoose.Schema.Types.ObjectId,
+   ref:'User',
+   required:true
+}
 },{
     timestamps:true
 })
