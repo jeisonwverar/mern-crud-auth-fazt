ce8d9f67a86a292f6ff9b357351b6d59aa5b8f53
Jeison Vera <jw.vera.r@gmail.com>
2023-11-30 21:11:54 -0500

    auth errors control

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -3,28 +3,42 @@ import{registerRequest} from '../api/auth.js';
 const AuthContext=createContext();
 export const useAuth=()=>{
  const context=useContext(AuthContext);
+
  if(!context){
     throw new Error('useAuth must be used within an AuthProvider');
  }
  return context;
 };
 export const AuthProvider=({children})=>{
-    const  [user, setUser] = useState(null);
-    const [isAuthenticated,setIsAuthenticated]=useState(false);
-    const singup=async(user)=>{
-        const res= await registerRequest(user);
-        console.log(res);
-        setUser(user);
-        setIsAuthenticated(true);
+    
+        const  [user, setUser] = useState(null);
+        const [isAuthenticated,setIsAuthenticated]=useState(false);
+        const [errors,setErrors]=useState([]);
 
-    }
+        const singup=async(user)=>{
+            try {
+
+            const res= await registerRequest(user);
+            console.log(res);
+            setUser(user);
+            setIsAuthenticated(true);
+        } catch (error) {
+            console.log(error.response)
+           setErrors(error.response.data)
+        }
+        }
+        
+
     return(
-
-<AuthContext.Provider value={{
-singup,
-user,
-}}>
+    
+    <AuthContext.Provider value={{
+    singup,
+    user,
+    isAuthenticated,
+    errors
+    }}>
     {children}
-</AuthContext.Provider>
+    </AuthContext.Provider>
     )
+
 };
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,10 +1,18 @@
 import{useForm} from 'react-hook-form';
-import {useAuth} from '../context/AuthContext'
-
+import {useAuth} from '../context/AuthContext';
+import { useEffect } from 'react';
+import {useNavigate} from 'react-router-dom';
 function RegisterPage() {
-    const {register, handleSubmit}=useForm()
-    const {singup,user}=useAuth();
- console.log(user);
+    const {register, handleSubmit,formState:{errors}}=useForm()
+    const {singup,isAuthenticated,errors:registerErrors}=useAuth();
+    const navigate = useNavigate()
+ 
+
+useEffect(()=>{
+  if(isAuthenticated) navigate('/tasks')
+},[isAuthenticated]);
+
+
 const onsubmit= handleSubmit(async(values)=>{
    singup(values);
     
@@ -14,18 +22,49 @@ const onsubmit= handleSubmit(async(values)=>{
 
   return (
     <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
-
+      {
+        registerErrors.map((error,i)=>(
+          <div className='bg-red-500 p-2 text-white text-center' key={i}> 
+            {error}
+          </div>
+        ))
+      }
    <form onSubmit={onsubmit}>
        <input type="text" 
        {...register('username',{required:true})} 
        className='w-full bg-zinc-700 text-white px-4 p-y2 rounded-md my-2'
        placeholder='username'/>
+
+        {
+          errors.username && (
+            <p className='text-red-500'>
+              username  is required
+            </p>
+          )
+        }
+
        <input type="email" {...register('email',{required:true})} 
        className='w-full bg-zinc-700 text-white px-4 p-y2 rounded-md my-2'
        placeholder='email'/>
+        {
+          errors.email && (
+            <p className='text-red-500'>
+              email  is required
+            </p>
+          )
+        }
+
        <input type="password" {...register('password',{required:true})}
        className='w-full bg-zinc-700 text-white px-4 p-y2 rounded-md my-2' 
        placeholder='password'/>
+
+{
+          errors.password && (
+            <p className='text-red-500'>
+              password is required
+            </p>
+          )
+        }
        <button type="submit">
         Register
        </button>
